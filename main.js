6f03f18a415022019e7c6cf6d6f64e9ac508a4e2
Farshad Shafii <farshad.shafii@gmail.com>
2020-11-27 15:34:05 +0000

    place the Date inside the updateTime function, placed the zeroToSeconds and zeroToMinutes function inside updateTime function, used setInterval to make the clock work automatically

--- main.js
+++ main.js
@@ -2,26 +2,20 @@ let hours = document.getElementById("hours");
 let minutes = document.getElementById("minutes");
 let seconds = document.getElementById("seconds");
 
-let today = new Date();
-
 function updateTime() {
+  let today = new Date();
+
   hours.innerText = today.getHours();
   minutes.innerText = today.getMinutes();
   seconds.innerText = today.getSeconds();
-}
 
-function addZeroToMinutes() {
   if (minutes.innerText < 10) {
     minutes.innerText = "0" + minutes.innerText;
   }
-}
 
-function addZeroToSeconds() {
   if (seconds.innerText < 10) {
     seconds.innerText = "0" + seconds.innerText;
   }
 }
 
-updateTime();
-addZeroToMinutes();
-addZeroToSeconds();
+setInterval(updateTime, 1000);
